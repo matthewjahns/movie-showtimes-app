2e76d4297f24fbe65e8699cd8dec7061fa4a8077
Matthew Jahns <matthew@Matthews-MacBook-Air.local>
2018-06-18 18:39:23 -0400

    Setup Repository

---
+++ showtimes.py
@@ -0,0 +1,22 @@
+import csv
+import json
+import os
+import pdb
+import requests
+import datetime
+import pytest
+import tkinter
+from IPython import embed
+
+## Movie Showtimes Application
+
+print("Welcome to Movie Showtimes!")
+##zip = 10003
+## TODO: add input for zip code
+##zip = input("Please enter the 5-digit zip code for your area")
+
+## TODO: add date functionality
+
+## TODO: add request to web
+
+## TODO
